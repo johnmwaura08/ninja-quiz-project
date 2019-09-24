eb5ddc1c59db35354a9d65b6d574ad845caf05bb
John Mwaura <johnmwaura08@gmail.com>
2019-09-23 21:14:56 -0500

    added result section and query selector

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
  const correctAnswers = ['B','B','B','B'];
  const form = document.querySelector('.quiz-form');
+ const result = document.querySelector('.result');
 
  form.addEventListener('submit', elem =>{
-     elem.preventDefault();
+     elem.preventDefault(); //preventing default action
 
 
     let score  =0;
@@ -15,7 +16,11 @@
             score += 25;
         }
     });
+//show result on page
+result.querySelector('span').textContent = `${score}%`; // this gets us the span in the result div
+result.classList.remove('d-none'); //we're now removing the d-none class which hides the result section. 
+//we dont have an onlcick because thesubmit is the eventlistner
 
-    console.log(score);
+
 
  } );
