bd2ca609b3c78b4ad45defda2ce058c86594389f
John Mwaura <johnmwaura08@gmail.com>
2019-09-23 20:33:36 -0500

    added event listener

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
- const correctAnswers = B
+ const correctAnswers = ['B','B','B','B'];
+ const form = document.querySelector('.quiz-form');
+
+ form.addEventListener('submit', elem =>{
+     elem.preventDefault();
+
+
+    let score  =0;
+    const userAnswers =[form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    //check answers
+
+    userAnswers.forEach((answer,index))
+
+
+
+ } );
